2422a885c710ac833427b08b62a0738d9f48f824
Ignacio Pomar Ballestero <tr.hiperion@gmail.com>
2025-09-23 18:27:58 +0200

    now the admin users has a independent class

--- zentrygate.php
+++ zentrygate.php
@@ -23,6 +23,7 @@ if (! defined ('ZENTRYGATE_DIR'))
 require_once ZENTRYGATE_DIR . 'ZentryGate/WpAdminPanel.php';
 require_once ZENTRYGATE_DIR . 'ZentryGate/AdminPanel/Dashboard.php';
 require_once ZENTRYGATE_DIR . 'ZentryGate/AdminPanel/Texts.php';
+require_once ZENTRYGATE_DIR . 'ZentryGate/AdminPanel/Users.php';
 require_once ZENTRYGATE_DIR . 'ZentryGate/Install.php';
 
 require_once ZENTRYGATE_DIR . 'ZentryGate/Plugin.php';
